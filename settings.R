6b73154d79993a1ce1f3cf707866b296e8522185
ndysle1 <65475763+ndysle1@users.noreply.github.com>
2021-02-22 09:28:35 -0500

    Adding end to settings script

--- settings.R
+++ settings.R
@@ -12,3 +12,6 @@ dir <- list()
 dir$project <- 'C:/Users/User/Documents/GitHub/Tableau/' 
 dir$scripts <- paste0(dir$project,'scripts/')
 dir$data    <- paste0(dir$project,'data/')
+
+
+# END ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
